7b8a74ba915feab5f96e4986b8ef82df2cdd287c
Nezha <783627014@qq.com>
2018-12-02 23:07:29 +0800

    加入打包脚本📦

---
+++ package.sh
@@ -0,0 +1,16 @@
+# build package into the wheel as below:
+# dist/
+#   pyquixote-0.0.1-py3-none-any.whl
+#   pyquixote-0.0.1.tar.gz
+
+python3 setup.py sdist bdist_wheel
+
+# twine upload dist/* to upload your package, and you should see output similar to this:
+# Uploading distributions to https://test.pypi.org/legacy/
+# Enter your username: [your username]
+# Enter your password:
+# Uploading pyquixote-0.0.1-py3-none-any.whl
+# 100%|█████████████████████| 4.65k/4.65k [00:01<00:00, 2.88kB/s]
+# Uploading pyquixote-0.0.1.tar.gz
+# 100%|█████████████████████| 4.25k/4.25k [00:01<00:00, 3.05kB/s]
+twine upload dist/*
